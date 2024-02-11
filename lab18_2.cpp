c7dff5e09148e252955fb0d12500f0dd978cd834
nulys nulys <nulys@gmasdsdil.com>
2024-02-11 21:44:12 +0700

    18.2

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,22 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm> 
+#include <cmath>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect {
+    double x, y, w, h;
 };
 
-double overlap(){
+double overlap(const Rect *r1, const Rect *r2) {
 
+    double left = max(r1->x, r2->x);
+    double right = min(r1->x + r1->w, r2->x + r2->w);
+    double top = min(r1->y, r2->y);
+    double bottom = max(r1->y - r1->h, r2->y - r2->h);
 
+    double width = max(0.0, right - left);
+    double height = max(0.0, top - bottom);
 
+    return width * height;
 }
+
